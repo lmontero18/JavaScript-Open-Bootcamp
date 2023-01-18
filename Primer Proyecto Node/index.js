ae08d29492de9a79527be1ce8cc3e50bedc14253
lmontero18 <lmonterom879@gmail.com>
2023-01-18 16:57:30 -0600

    new changes

---
+++ Primer Proyecto Node/index.js
@@ -0,0 +1,12 @@
+//Este es el mensaje de bienvenida
+
+console.log("Puerta de entrada al proyecto")
+
+/* 
+Todo dentro de esto, es un 
+comentario
+*/
+
+//Esta es el mensaje de salida
+
+console.log("Adios")
