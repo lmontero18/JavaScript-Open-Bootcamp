e24fcaa49fa0d346e012341503c4899f7bbf8f10
lmontero18 <lmonterom879@gmail.com>
2023-01-20 20:18:03 -0600

    else if, switch

---
+++ sesion03-Estructuras-de-control/Sentencias-Switch.js
@@ -0,0 +1,25 @@
+//Sentencias Switch
+let nota = 5;
+
+switch (nota) {
+    case 5: 
+        console.log("Excelente");
+        break;
+    case 4:
+        console.log("Buen trabajo");
+        break;
+    case 3:
+        console.log("Deficiente");
+        break;
+    case 2:
+        console.log("Malo");
+        break;
+    case 1:
+        console.log("Pesimo");
+        break;
+
+    default: 
+        console.log("Error")
+        break;
+}
+
---
+++ sesion03-Estructuras-de-control/else if.js
@@ -0,0 +1,18 @@
+//if else + if else
+
+let nota = 5;
+
+if ( nota === 5) { 
+    console.log("Sobresaliente")
+} else if ( nota === 4){
+    console.log("Bueno")
+} else if ( nota === 3){
+    console.log("Suficiente")
+} else if ( nota === 2){
+    console.log("Denegado")
+} else if ( nota === 1){
+    console.log("Denegado")
+} else { 
+    console.log("Error --- Nota menor o igual a 0 / Nota mayor igual a 6");
+}
+
