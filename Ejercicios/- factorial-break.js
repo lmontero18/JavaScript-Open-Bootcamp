5aca63711e31ad8307a4c1c1e9bbb8ba1ccd82e4
lmontero18 <lmonterom879@gmail.com>
2023-02-01 15:21:53 -0600

    sesion04-Strings

---
+++ Ejercicios/- factorial-break.js
@@ -0,0 +1,11 @@
+let num = 1;
+let total = 0;
+
+
+while(num <=10){
+    total = total * 10;   
+
+    if(num >= 10){
+        break;
+    }
+}
---
+++ Ejercicios/Strings.js
@@ -0,0 +1,9 @@
+// String y como trabajar con ellos 
+
+let str_dbl= "Hola soy un texto con comillas dobles";
+let str_sng = 'Texto con comillas simples';
+
+console.log(str_dbl);
+console.log(str_sng);
+
+
